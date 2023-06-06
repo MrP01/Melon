7e635f2e761377c0dade4e39f510b00a1885cb2b
MrP01 <peter@waldert.at>
2023-06-06 01:43:02 +0100

    chips are now green

--- gui/tasklist.py
+++ gui/tasklist.py
@@ -8,8 +8,9 @@ class TaskItemDelegate(QItemDelegate):
     def paint(self, painter: QPainter, option: QStyleOptionViewItem, index: QModelIndex | QPersistentModelIndex):
         todo = index.data(Qt.ItemDataRole.UserRole)
         rect: QRect = option.rect
-
         originalPen = painter.pen()
+        originalFont = painter.font()
+
         path = QPainterPath()
         path.addRoundedRect(rect.marginsRemoved(QMargins(2, 2, 2, 2)), 6, 6)
         painter.setPen(QPen(QColor(0, 255, 0, 150)))
@@ -19,13 +20,14 @@ class TaskItemDelegate(QItemDelegate):
             painter.fillPath(path, QColor(200, 200, 200, 50))
 
         path = QPainterPath()
-        path.addRoundedRect(QRect(rect.x() + 4, rect.y() + 22, len(todo.contents["calendar"]) * 8 + 12, 20), 10, 10)
+        path.addRoundedRect(QRect(rect.x() + 4, rect.y() + 25, len(todo.contents["calendar"]) * 8 + 12, 16), 10, 10)
         painter.drawPath(path)
+        painter.setFont(QFont("Monospace", 9))
+        painter.drawText(rect.translated(12, 26), todo.contents["calendar"])
 
         painter.setPen(originalPen)
-        color = Qt.GlobalColor.black
-        QApplication.style().drawItemText(painter, rect.translated(8, 3), 0, color, True, index.data())
-        QApplication.style().drawItemText(painter, rect.translated(12, 23), 0, color, True, todo.contents["calendar"])
+        painter.setFont(originalFont)
+        painter.drawText(rect.translated(8, 3), index.data())
 
     def sizeHint(self, option: QStyleOptionViewItem, index: QModelIndex) -> QSize:
         return QSize(100, 50)
